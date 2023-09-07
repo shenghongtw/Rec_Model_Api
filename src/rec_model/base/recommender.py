6cedc29cfc0d301cb2ba33194826a6be217a96b2
Rory <roryhaung1@gmail.com>
2023-09-07 23:24:58 +0800

    add check_rating_boundary func

--- src/rec_model/base/recommender.py
+++ src/rec_model/base/recommender.py
@@ -1,4 +1,5 @@
-
+from metric.rating import rating_measure
+from metric.ranking import ranking_measure
 class Recommeder:
     def __init__(self, train_set, test_set):
         self.train_set = train_set
@@ -19,11 +20,13 @@ class Recommeder:
     def predict_score(self, u, i):
         pass
 
-    def predict_rank(self, u):
-        pass
-    
     def check_rating_boundary(self,prediction):
-        pass
+        if prediction > self.data.rScale[-1]:
+            return self.data.rScale[-1]
+        elif prediction < self.data.rScale[0]:
+            return self.data.rScale[0]
+        else:
+            return round(prediction,3)
         
     def eval_rating(self):
         pass
