1f036e5e617ca74ba655f9b0a4eb6fb8c62bd79a
Rory <roryhaung1@gmail.com>
2023-09-11 22:25:47 +0800

    add evaluate file

---
+++ rec_model/evaluate/evaluate.py
@@ -0,0 +1,39 @@
+from rec_model.evaluate.metrics import *
+
+
+def eval_pos_neg(model, test_data, metric_names, k=10, batch_size=None):
+    """Evaluate the performance of Top-k recommendation algorithm.
+    Note: Test data must contain some negative samples(>= k) and one positive samples.
+    Args:
+        :param model: A model built-by tensorflow.
+        :param test_data: A dict.
+        :param metric_names: A list like ['hr'].
+        :param k: A scalar(int).
+        :param batch_size: A scalar(int).
+    :return: A result dict such as {'hr':, 'ndcg':, ...}
+    """
+    pred_y = - model.predict(test_data, batch_size)
+    return eval_rank(pred_y, metric_names, k)
+
+
+def eval_rank(pred_y, metric_names, k=10):
+    """Evaluate
+        Args:
+            :param pred_y: A ndarray.
+            :param metric_names: A list like ['hr'].
+            :param k: A scalar(int).
+        :return: A result dict such as {'hr':, 'ndcg':, ...}
+    """
+    rank = pred_y.argsort().argsort()[:, 0]
+    res_dict = {}
+    for name in metric_names:
+        if name == 'hr':
+            res = hr(rank, k)
+        elif name == 'ndcg':
+            res = ndcg(rank, k)
+        elif name == 'mrr':
+            res = mrr(rank, k)
+        else:
+            break
+        res_dict[name] = res
+    return res_dict
