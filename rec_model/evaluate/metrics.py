000c8f30a3b639f0f87e9d8725a7acd72f29829f
Rory <roryhaung1@gmail.com>
2023-09-11 22:25:57 +0800

    add metrics file

---
+++ rec_model/evaluate/metrics.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+def hr(rank, k):
+    """Hit Rate.
+    Args:
+        :param rank: A list.
+        :param k: A scalar(int).
+    :return: hit rate.
+    """
+    res = 0.0
+    for r in rank:
+        if r < k:
+            res += 1
+    return res / len(rank)
+
+
+def mrr(rank, k):
+    """Mean Reciprocal Rank.
+    Args:
+        :param rank: A list.
+        :param k: A scalar(int).
+    :return: mrr.
+    """
+    mrr = 0.0
+    for r in rank:
+        if r < k:
+            mrr += 1 / (r + 1)
+    return mrr / len(rank)
+
+
+def ndcg(rank, k):
+    """Normalized Discounted Cumulative Gain.
+    Args:
+        :param rank: A list.
+        :param k: A scalar(int).
+    :return: ndcg.
+    """
+    res = 0.0
+    for r in rank:
+        if r < k:
+            res += 1 / np.log2(r + 2)
+    return res / len(rank)
